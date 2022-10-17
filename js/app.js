5d5ccc1aa7f49dffb725537e101ebeb1d4df3410
purplena <lkhramova14@gmail.com>
2022-10-17 14:11:34 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,46 @@
+// ********** set date ************
+// select span
+var date = document.getElementById("date");
+
+if (date) {
+  date.innerHTML = new Date().getFullYear();
+}
+
+// document.getElementById("date").addEventListener("click", () => {
+//   document.getElementById("date").style.color = "red";
+// });
+
+// console.log(document.getElementById("date").outerHTML);
+// console.log(document.getElementById("date").style.color);
+
+// ********** nav toggle ************
+// select button and links
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-links");
+// add event listener
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-links");
+});
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    links.classList.remove("show-links");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    //
+    let position = element.offsetTop - 62;
+
+    window.scrollTo({
+      left: 0,
+      // top: element.offsetTop,
+      top: position,
+      behavior: "smooth",
+    });
+  });
+});
